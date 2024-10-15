0264ccb95476b6c3d292339ea89057a8d60fba2a
Thomas Sweeney <twsweeney13@gmail.com>
2024-10-15 16:02:39 -0400

    complete structs section

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,27 +1,33 @@
-#[derive(Debug)] // Adding this outer attribute allows for debugging information to be printed 
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
 }
 
-fn main() {
+impl Rectangle {
+    // fn area(&self) -> u32 {
+    //     self.width * self.height
+    // }
 
-    let scale = 2;
-    let rect1 = Rectangle {
-        width: dbg!(30*scale),
-        height: 50,
-    };
-
-
-    dbg!(&rect1);
-    // println!("rect1 is {:#?}", rect1);
-
-    println!(
-        "The area of the rectangle is {} square pixels", 
-        area(&rect1)
-    );
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
 }
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
