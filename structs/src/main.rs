22a48e2ba85c3586fc6610dc04aef5c26e7a8e70
Thomas Sweeney <twsweeney13@gmail.com>
2024-10-15 15:36:45 -0400

    complete structs

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,3 +1,27 @@
+#[derive(Debug)] // Adding this outer attribute allows for debugging information to be printed 
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    let scale = 2;
+    let rect1 = Rectangle {
+        width: dbg!(30*scale),
+        height: 50,
+    };
+
+
+    dbg!(&rect1);
+    // println!("rect1 is {:#?}", rect1);
+
+    println!(
+        "The area of the rectangle is {} square pixels", 
+        area(&rect1)
+    );
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
